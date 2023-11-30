26568b17a2e30432c9794285a6f64b987fed49fe
julieelder <102327795+julieelder@users.noreply.github.com>
2023-11-30 09:04:25 -0600

    Update streamlit_app.py
    cleaning up

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-# import sqlite3
+from urllib.error import URLError
 
 # conn = sqlite3.connect('PC_RIVERY_DB.PUBLIC')
 # c = conn.cursor()
@@ -17,6 +17,7 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+#import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -32,6 +33,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 # add a text entry box
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
+
+#import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 #streamlit.text(fruityvice_response.json())
 
@@ -40,6 +43,8 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # and put it into a table
 streamlit.dataframe(fruityvice_normalized)
 
+# don't run anything past here while we troubleshoot
+streamlit.stop()
 # adding a text box for fruit list
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
